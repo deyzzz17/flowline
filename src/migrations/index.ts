2e833ab7ca0176883f8e245645e9c362a3d86427
deyzzz17 <swodey@free.fr>
2026-03-24 20:00:28 +0100

    update

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,9 +1,21 @@
 import * as migration_20260319_110814 from './20260319_110814';
+import * as migration_20260324_184033 from './20260324_184033';
+import * as migration_20260324_185931 from './20260324_185931';
 
 export const migrations = [
   {
     up: migration_20260319_110814.up,
     down: migration_20260319_110814.down,
-    name: '20260319_110814'
+    name: '20260319_110814',
+  },
+  {
+    up: migration_20260324_184033.up,
+    down: migration_20260324_184033.down,
+    name: '20260324_184033',
+  },
+  {
+    up: migration_20260324_185931.up,
+    down: migration_20260324_185931.down,
+    name: '20260324_185931'
   },
 ];
