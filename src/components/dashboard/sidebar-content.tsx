490886a4d887c5da83badc86212ff0d4f3a65f37
deyzzz17 <swodey@free.fr>
2026-04-12 21:25:54 +0200

    update sidebar

--- src/components/dashboard/sidebar-content.tsx
+++ src/components/dashboard/sidebar-content.tsx
@@ -1,8 +1,7 @@
 'use client'
 
-import { ClipboardList } from 'lucide-react'
+import { ClipboardList, Home } from 'lucide-react'
 import { NavItem } from './nav-item'
-import { SidebarProfile } from './sidebar-profile'
 
 interface SidebarContentProps {
   onNavigate?: () => void
@@ -10,11 +9,11 @@ interface SidebarContentProps {
 
 export const SidebarContent = ({ onNavigate }: SidebarContentProps) => {
   return (
-    <div className="flex flex-1 flex-col justify-between p-3">
+    <div className="flex flex-1 flex-col p-3">
       <nav className="space-y-1">
+        <NavItem href="/dashboard" icon={Home} label="Home" onNavigate={onNavigate} />
         <NavItem href="/lists" icon={ClipboardList} label="Tasks" onNavigate={onNavigate} />
       </nav>
-      <SidebarProfile onNavigate={onNavigate} />
     </div>
   )
 }
