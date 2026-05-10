e1a22cbc0c202c1fe01cedacec25bfb29856491a
deyzzz17 <swodey@free.fr>
2026-05-10 20:08:02 +0200

    update inngest functions

--- src/app/api/inngest/route.ts
+++ src/app/api/inngest/route.ts
@@ -1,8 +1,10 @@
 import { serve } from 'inngest/next'
 import { inngest } from '@/lib/inngest'
 import { syncRecurringTasks } from '@/inngest/functions/sync-recurring-tasks'
+import { autoDeleteExpiredTasks } from '@/inngest/functions/auto-delete-expired-tasks'
+import { cleanupTrashedTasks } from '@/inngest/functions/cleanup-trashed-task'
 
 export const { GET, POST, PUT } = serve({
   client: inngest,
-  functions: [syncRecurringTasks],
+  functions: [syncRecurringTasks, autoDeleteExpiredTasks, cleanupTrashedTasks],
 })
