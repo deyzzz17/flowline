bc50db223e1a820d77afb5987c7b152af9d4fd4d
deyzzz17 <swodey@free.fr>
2026-05-13 13:17:48 +0200

    update lists

--- src/app/(frontend)/(app)/lists/new-list/page.tsx
+++ src/app/(frontend)/(app)/lists/new-list/page.tsx
@@ -4,7 +4,7 @@ import { ProtectedRoute } from '@/components/route/protected-route'
 export default async function NewListPage() {
   return (
     <ProtectedRoute>
-      <div className="relative mx-auto px-4 pb-16 sm:px-6 lg:px-10">
+      <div className="relative px-4 pb-16 sm:px-6 lg:px-10">
         <div className="relative z-10">
           <NewListClient />
         </div>
