68bb86c87d1ba5bdb7182fad2028768f04501671
deyzzz17 <swodey@free.fr>
2026-06-19 17:12:54 +0200

    update contacts page

--- src/app/(frontend)/(app)/contacts/page.tsx
+++ src/app/(frontend)/(app)/contacts/page.tsx
@@ -6,7 +6,7 @@ export default async function ContactsPage() {
   const [, initialData] = await Promise.all([requireAuth(), getContactsPageData()])
 
   return (
-    <div className="mx-auto max-w-2xl px-4 pb-16 pt-8 sm:px-6 lg:px-10">
+    <div className="relative px-4 pb-16 pt-8 sm:px-6 lg:px-10">
       <ContactsClient initialData={initialData} />
     </div>
   )
